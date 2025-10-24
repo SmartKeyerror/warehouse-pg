d1579c869cf3a741ad3a5ba0db98d5a2b36d9d89
Artem Shapatin <a.shapatin@arenadata.io>
2025-10-24 20:45:39 +0700

    Planner issue that leads to rescan of the Motion (#94)
    Fix issue with rescan of Motion
    
    In some cases on path creation stage, the planner didn't account, that
    the outer relation of a join in case of parametrization by outer
    parameter from join above, might be rescanned.
    
    Example of approximate plans that planner was able to produce, which lead to error.
    HashJoin plan
    
    Gather Motion
        ->  Nested Loop
                ->  Broadcast Motion
                    ->  Seq Scan (we get param here)
                ->  Materialize
                    ->  Hash Join
                            ->  Broadcast Motion
                                ->  Foreign Scan
                            ->  Hash
                                ->  Function Scan (we use param here)
    
    MergeJoin plan
    
    Nested Loop Semi Join
       ->  Gather Motion
             ->  Seq Scan
       ->  Materialize
             ->  Merge Left Join
                   ->  Gather Motion
                         ->  Sort
                               ->  Seq Scan
                   ->  Sort
                         ->  Result
                               Filter (we use param here)
                               ->  Materialize
                                     ->  Gather Motion
                                           ->  Seq Scan
    
    In HashJoin we are getting error when we try to rescan Foreign Scan. We do it
    because Seq Scan can produce >1 rows so, because of parametrization
    we will be forced to rebuild HashTable and rescan Foreign Scan too.
    And in MergeJoin we are getting error when we try to rescan Gather Motion of
    outer side of Merge Join, for the same reason.
    
    This patch applies same fix, previously used to address same problem
    for NestLoop Join.
    After creating a motions in join, we check if we have outer
    parameters, which can lead to rescanning of outer path, and
    if we do have them and we're not rescanable, we add Materialization
    to outer path. We do not apply this logic for inner paths because they
    are already rescannable by themselves in some form.

--- src/test/regress/sql/bfv_joins.sql
+++ src/test/regress/sql/bfv_joins.sql
@@ -595,6 +595,61 @@ drop table l_table;
 drop table r_table1;
 drop table r_table2;
 
+-- Test case for hash join rescan with multiple batches
+create table outer_table (o int) distributed replicated;
+create table inner_table (i1 int, i2 int) distributed replicated;
+create table loops_table (x int) distributed replicated;
+-- Outer table must have skew (values < 20 are much more common)
+insert into outer_table select i%1000 from generate_series(1, 100000) i;
+insert into outer_table select i%20 from generate_series(1, 10000) i;
+insert into inner_table select i%10000, i%10000 from generate_series(1, 50000) i;
+analyze outer_table;
+analyze inner_table;
+
+-- The order of values is important, since only the first one is
+-- processed before any rescans.
+--
+-- Values < 20 are placed into the skew table, values >= 20 into
+-- the main hashtable. The specific values >= 20 are chosen to be
+-- in different batches (according to their hashes).
+--
+-- Analyze is called after the first value to trick planner into
+-- using Nested loop, by pretending we only have 1 row here.
+insert into loops_table values (11);
+analyze loops_table;
+insert into loops_table select generate_series(101, 105);
+insert into loops_table values (14), (15);
+
+-- Ensure statistics are what we expect to see
+select reltuples from pg_class where relname = 'loops_table';
+select count(*) from
+  (select unnest(most_common_vals::text::int[])
+   from pg_stats where tablename='outer_table') t(v)
+where v in (11, 14, 15);
+
+set statement_mem to "1MB";
+set optimizer to off;
+set enable_nestloop to on;
+
+-- We want to have Hash Join inside Nested Loop here
+-- This is needed so that Hash Join is rescanned multiple times.
+explain analyze select * from loops_table where x in (select i2 from outer_table join inner_table on o = i1);
+
+set gp_workfile_compression=off;
+select * from loops_table where x in (select i2 from outer_table join inner_table on o = i1);
+
+set gp_workfile_compression=on;
+select * from loops_table where x in (select i2 from outer_table join inner_table on o = i1);
+
+reset gp_workfile_compression;
+reset statement_mem;
+reset optimizer;
+reset enable_nestloop;
+
+drop table outer_table;
+drop table inner_table;
+drop table loops_table;
+
 -- Should throw an error during planning: FULL JOIN is only supported with merge-joinable or hash-joinable join conditions
 -- Falls back on GPORCA, but shouldn't cause GPORCA to crash
 CREATE TABLE ext_stats_tbl(c0 name, c2 boolean);
@@ -604,6 +659,108 @@ INSERT INTO ext_stats_tbl VALUES('tC', true);
 ANALYZE ext_stats_tbl;
 
 explain SELECT 1 FROM ext_stats_tbl t11 FULL JOIN ext_stats_tbl t12 ON t12.c2;
+
+-- This test is introduced to verify that outer side of HashJoin path
+-- is rescannable, if this HashJoin is parametrized by the inner side of
+-- another join.
+--start_ignore
+drop table if exists ot cascade;
+drop foreign table if exists w cascade;
+drop function if exists foo(i int);
+--end_ignore
+create table ot (i int);
+insert into ot values (1), (1);
+
+create external web table w (i int) execute 'echo 1' on all format 'text';
+
+create function foo(i int)
+returns table (i int)
+language sql
+execute on all segments
+strict
+rows 30000
+cost 100000
+as $$
+    select 1;
+$$;
+
+set enable_nestloop = 'on';
+set enable_mergejoin = 'off';
+set enable_hashjoin = 'on';
+set from_collapse_limit = '1';
+set join_collapse_limit = '1';
+
+explain (verbose, costs off) select 1
+from ot,
+     foo(ot.i) as f
+join w on w.i = f.i;
+
+reset enable_nestloop;
+reset enable_mergejoin;
+reset enable_hashjoin;
+reset from_collapse_limit;
+reset join_collapse_limit;
+
+-- This test is introduced to verify that outer side of MergeJoin path
+-- is rescannable, if this MergeJoin is parametrized by the inner side of
+-- another join.
+--start_ignore
+drop table if exists a, b, t1, t2 cascade;
+--end_ignore
+
+create table t1(a int, b int);
+create table t2(a int);
+create table a (i int4);
+create table b (i int4);
+
+insert into t1 values (1, 99), (1, 99);
+insert into t2 values (1), (1);
+insert into a values (1);
+insert into b values (1);
+
+analyze t1;
+analyze t2;
+analyze a;
+analyze b;
+
+set allow_system_table_mods = on;
+
+update pg_class set reltuples=10000000 where oid ='a'::regclass;
+update pg_class set relpages=10000 where oid ='a'::regclass;
+
+update pg_class set reltuples=1000000 where oid ='b'::regclass;
+update pg_class set relpages=1000 where oid ='b'::regclass;
+
+set join_collapse_limit=1;
+set from_collapse_limit=1;
+
+set enable_mergejoin=on;
+set enable_hashjoin=off;
+
+explain (verbose, costs off) select *
+from t1 out
+where out.b in (select t1.a
+                from (select a.i as a from a order by a) t1 left outer join (select a
+                                             from t2
+                                             where t2.a = out.b
+                                             order by a) t2_d on t1.a=t2_d.a);
+
+reset join_collapse_limit;
+reset from_collapse_limit;
+reset enable_mergejoin;
+reset enable_hashjoin;
+reset allow_system_table_mods;
+-- test that empty table gets hashed instead of a bigger table
+create table big_table(s smallint) distributed replicated;
+create table empty_table(i int) distributed replicated;
+insert into big_table select generate_series(1, 10000);
+analyze big_table;
+analyze empty_table;
+
+explain select * from big_table, empty_table where s = i;
+
+drop table big_table;
+drop table empty_table;
 
 -- Clean up. None of the objects we create are very interesting to keep around.
 reset search_path;
